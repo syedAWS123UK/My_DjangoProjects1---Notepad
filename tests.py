8212907498a17f0223966ecd11503781d0a3b9c1
Syed Fareed Ahmed <syedahmed261@gmail.com>
2023-09-11 02:18:46 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
 import sys
-sys.path.append('/user/computer/project/django/notepad')
+sys.path.append('/user/computer/project/django/notepad\notepad_app')
 
 # Create your tests here.
